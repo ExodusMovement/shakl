996bd4a9edd3dd207d5bb6708331630118c5776a
a <sonayen.a@gmail.com>
2018-05-13 13:33:06 +0300

    Support attrs

--- styled.js
+++ styled.js
@@ -3,33 +3,48 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 // TODO:
-// support attrs
+// support styled(Styled)
+// support isStyled
+// support prop-types
+// support style types
 // support extend
 // support extend attrs
-// support styled(Styled)
 
-const styled = Component => (...styles) => ({
-  children,
-  innerRef,
-  ...props
-}) => {
-  const ref = innerRef ? { ref: r => innerRef(r) } : {};
+const styled = (Component, { withAttrs = {} } = {}) => {
+  const StyledComponentFactory = (...styles) => {
+    const StyledComponent = ({ innerRef, ...props }) => {
+      const ref = innerRef
+        ? { ref: typeof innerRef === 'function' ? r => innerRef(r) : innerRef }
+        : {};
 
-  const style = [
-    props.style,
-    styles.map(s => (typeof s === 'function' ? s(props) : s))
-  ];
+      const style = [
+        props.style,
+        withAttrs.style,
+        styles.map(s => (typeof s === 'function' ? s(props) : s))
+      ];
 
-  const StyledComponent = React.createElement(
-    Component,
-    { ...props, ...ref, style },
-    children
-  );
+      const createComponent = React.isValidElement(Component)
+        ? React.cloneElement
+        : React.createElement;
 
-  const displayName = Component.displayName || Component.name || 'Component';
-  StyledComponent.displayName = `Styled${displayName}`;
+      return createComponent(Component, {
+        ...props,
+        ...withAttrs,
+        ...ref,
+        style
+      });
+    };
 
-  return StyledComponent;
+    const displayName = Component.displayName || Component.name || 'Component';
+    StyledComponent.displayName = `Styled${displayName}`;
+
+    return StyledComponent;
+  };
+
+  StyledComponentFactory.attrs = props =>
+    styled(Component, { withAttrs: props });
+
+  return StyledComponentFactory;
 };
 
 styled.Text = styled(Text);
