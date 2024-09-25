548a4083d980bfde7744aa1e97364750eb09109e
Vodopyanov Egor <egorvodopyanov@gmail.com>
2024-09-25 18:59:04 +0300

    make types generic

--- src/index.d.ts
+++ src/index.d.ts
@@ -12,29 +12,46 @@ import type {
 export type AnyStyle = ViewStyle | TextStyle | ImageStyle;
 export type AnyProps = ViewProps | TextProps | TouchableOpacityProps | ImageProps;
 
-export type ViewStyleFunction = (props: ViewProps) => ViewStyle;
-export type TextStyleFunction = (props: TextProps) => TextStyle;
-export type ImageStyleFunction = (props: ImageProps) => ImageStyle;
-export type TouchableStyleFunction = (props: TouchableOpacityProps) => ViewStyle;
-export type AnyStyleFunction = (props: AnyProps | any) => AnyStyle;
+// Make the style functions generic
+export type ViewStyleFunction<P = ViewProps> = (props: P) => ViewStyle;
+export type TextStyleFunction<P = TextProps> = (props: P) => TextStyle;
+export type ImageStyleFunction<P = ImageProps> = (props: P) => ImageStyle;
+export type TouchableStyleFunction<P = TouchableOpacityProps> = (props: P) => ViewStyle;
+export type AnyStyleFunction<P = any> = (props: P) => AnyStyle;
 
-export type StyledComponentType = ComponentType<any> & {
-  extend: (more: AnyStyle | AnyStyleFunction) => StyledComponentType;
-  attrs: (attrs: any) => StyledComponentType;
-  withComponent: (comp: ComponentType<any>) => StyledComponentType;
+// Make StyledComponentType generic in P
+export type StyledComponentType<P = any> = ComponentType<P> & {
+  extend: (more: AnyStyle | AnyStyleFunction<P>) => StyledComponentType<P>;
+  attrs: (attrs: Partial<P> | AnyStyleFunction<P>) => StyledComponentType<P>;
+  withComponent: (comp: ComponentType<any>) => StyledComponentType<P>;
   withChild: (
     child: ComponentType<any>,
     childProps?: any
-  ) => StyledComponentType;
+  ) => StyledComponentType<P>;
 };
 
+// Make the Styled interface generic
 export interface Styled {
-  (Component: ComponentType<any>, config?: any): (style: AnyStyle | AnyStyleFunction) => StyledComponentType;
+  <P = any>(
+    Component: ComponentType<P>,
+    config?: any
+  ): (style: AnyStyle | AnyStyleFunction<P>) => StyledComponentType<P>;
 
-  View: (style: ViewStyle | ViewStyleFunction) => StyledComponentType;
-  Text: (style: TextStyle | TextStyleFunction) => StyledComponentType;
-  Image: (style: ImageStyle | ImageStyleFunction) => StyledComponentType;
-  Touchable: (style: ViewStyle | TouchableStyleFunction) => StyledComponentType;
+  View: <P = ViewProps>(
+    style: ViewStyle | ViewStyleFunction<P>
+  ) => StyledComponentType<P>;
+  Text: <P = TextProps>(
+    style: TextStyle | TextStyleFunction<P>
+  ) => StyledComponentType<P>;
+  Image: <P = ImageProps>(
+    style: ImageStyle | ImageStyleFunction<P>
+  ) => StyledComponentType<P>;
+  Touchable: <P = TouchableOpacityProps>(
+    style: ViewStyle | TouchableStyleFunction<P>
+  ) => StyledComponentType<P>;
+  LinearGradient: <P = ViewProps>(
+    style: ViewStyle | ((props: P) => ViewStyle)
+  ) => StyledComponentType<P>;
 }
 
 declare const styled: Styled;
