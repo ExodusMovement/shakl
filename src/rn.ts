3bd38d70b7ee2d2c8cf856a1c32f0dadc9a4f4a4
Vodopyanov Egor <egorvodopyanov@gmail.com>
2024-10-18 23:54:27 +0300

    fix: textInput generic

--- src/rn.ts
+++ src/rn.ts
@@ -21,7 +21,7 @@ const extended = Object.assign(styled,{
   Text: styled<TextProps, TextStyle>(Text, { name: 'styled(Text)' }),
   Image: styled<ImageProps, ImageStyle>(Image, { name: "styled(Image)" }),
   Touchable: styled<TouchableOpacityProps, ViewStyle>(TouchableOpacity, { name: 'styled(Touchable)' }),
-  TextInput: styled<TextInputProps, ViewStyle>(TextInput, { name: 'styled(TextInput)' }),
+  TextInput: styled<TextInputProps, TextStyle>(TextInput, { name: 'styled(TextInput)' }),
 })
 
 export default extended;
