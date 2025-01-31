d3e10a39d158da0c37c617ee2121e669982acee3
Alexander Gazizov <alexander.ga@exodus.io>
2025-01-31 13:41:02 +0200

    feat: add omitProps config with default value to omit theme prop passing

--- test/props.test.js
+++ test/props.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { View } from 'react-native'
 import { create as r } from 'react-test-renderer'
 
 import s from '../src/rn'
@@ -8,6 +9,32 @@ it('accepts a style prop', () => {
   const foo = r(<Foo style={{ flex: 1 }} />).toJSON()
 
   expect(foo.props.style).toEqual({ margin: 10, flex: 1 })
+
+  expect(foo).toMatchSnapshot()
+})
+
+const theme = {
+  fancyColor: '#193bc3',
+}
+
+it("doesn't pass `theme` property by default", () => {
+  const Foo = s.View(({ theme }) => ({ color: theme.fancyColor }))
+  const foo = r(<Foo theme={theme} />).toJSON()
+
+  expect(foo.props.style).toEqual({
+    color: '#193bc3',
+  })
+
+  expect(foo).toMatchSnapshot()
+})
+
+it('passes `theme` property if `omitProps` specified without it', () => {
+  const Foo = s(View, { omitProps: [] })(({ theme }) => ({ color: theme.fancyColor }))
+  const foo = r(<Foo theme={theme} />).toJSON()
+
+  expect(foo.props.style).toEqual({
+    color: '#193bc3',
+  })
 
   expect(foo).toMatchSnapshot()
 })
